f7153891dd0f9eb231e143328995dffcdb09ad32
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-03-30 19:18:11 +0700

    quicksort

--- QuickSort_102/QuickSort_102.cpp
+++ QuickSort_102/QuickSort_102.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int arr[20];	//array of integers to hold values
-int cmp_count = 0; // 
-int mov_count = 0; // number of comparasion
+int cmp_count = 0; // number of comparassion
+int mov_count = 0; // number of data movement
 int n; 
 
 void input() {
@@ -54,7 +54,64 @@ void q_short(int low, int high)
 
 	while (i <= j)			//Langkah Algoritma No.10
 	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))		//Langkah Algoritma No.5
+		{
+			i++;										//Langkah Algoritma No.6
+			cmp_count++;
+		}
+		cmp_count++;
+
 		//search for an element less than or equal to pivot
-		while ((arr[i] <= pivot) && (j <= high))		//Langkah Algoritma No.5
+		while ((arr[j] > pivot) && (j >= low))		//Lngkah Algoritma No.7
+		{
+			j--;									//Langkah Algoritma No.8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//if the greater element is on the left of the element
+		if (i < j)									//Langkah Algoritma No.9
+		{
+			//swap the element at index i with the element at index j
+			swap(i, j);
+			mov_count++;
+		}
 	}
+	//now containt the index of the last element in the sorted list
+	if (low < j)									//Langkah Algoritm No.11
+	{
+		//move the pivot to its correct position in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of pivot using quick sort
+	q_short(low, j - 1);							//Langkah Algoritma No.12
+
+	//sort the list on the right of pivot using quick sort
+	q_short(j + 1, high);							//Langkah Algortma No.13
+}
+
+void display() {
+	cout << "\n===============" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "\n===============" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of Comparassions: " << cmp_count << endl;
+	cout << "Number of data movement: " << mov_count << endl;
+}
+
+int main()
+{
+	input();
+		q_short(0, n - 1);						//Sort the array using quick sort
+		display();
+		system("pause");
+
+		return 0;
 }
