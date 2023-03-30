5ab859f35dad246575cf58b33e18f715005f1fca
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-03-30 15:09:54 +0700

    membuat input

--- QuickSort_102/QuickSort_102.cpp
+++ QuickSort_102/QuickSort_102.cpp
@@ -2,4 +2,29 @@
 using namespace std;
 
 int arr[20];	//array of integers to hold values
+int cmp_count = 0 // 
 int mov_count = 0; // number of comparasion
+int n; 
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n====================" << endl;
+	cout << "\nEnter Array element" << endl;
+	cout << "\n====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
